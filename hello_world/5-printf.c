cb9e9d2e3c20c417af76b97e78c455404337c575
Taisa456 <bakhysheva2004@gmail.com>
2025-02-25 13:35:09 +0000

    5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints "with proper grammar, but the outcome is a piece of art,"
+ * followed by a new line.
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
